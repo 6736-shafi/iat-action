b48062051175c2cbcad66a91024c4b9f5e1e2f76
md shafi uddin <mdshafi.uddin@cdw.com>
2025-06-16 22:01:14 +0530

    a

--- src/register_model_component.py
+++ src/register_model_component.py
@@ -89,11 +89,5 @@ if __name__ == "__main__":
     )
 
 
-from src.utils.get_model_path import get_latest_model_from_folder
-
-latest_model = get_latest_model_from_folder('./model')
-print(f"Latest model found: {latest_model}")
-latest_model='../model/GBM_model_python_1749740346516_6.zip'
-print(f"Full path to latest model: {latest_model}")
 
 
